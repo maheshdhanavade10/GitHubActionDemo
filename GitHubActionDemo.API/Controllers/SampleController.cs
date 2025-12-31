bb6fef89945f6e76ea8e03e3b02dd9b2ed09c984
maheshdhanavade10 <mahesh.dhanavade@gmail.com>
2025-12-31 15:16:54 +0530

    sql injection test.

--- GitHubActionDemo.API/Controllers/SampleController.cs
+++ GitHubActionDemo.API/Controllers/SampleController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 
 namespace GitHubActionDemo.API.Controllers
 {
@@ -14,5 +15,24 @@ namespace GitHubActionDemo.API.Controllers
             //OK
             return Ok(new { Message = "Auth test successful" });
         }
+
+        // ---------------------------------------------------------
+        // 1. SQL Injection
+        // ---------------------------------------------------------
+        [HttpGet("user")]
+        public IActionResult GetUser(string username)
+        {
+            // ❌ Vulnerable: SQL Injection
+            string query = "SELECT * FROM Users WHERE Username = '" + username + "'";
+
+            using var conn = new SqlConnection("Server=.;Database=Test;Trusted_Connection=True;");
+            using var cmd = new SqlCommand(query, conn);
+
+            conn.Open();
+            cmd.ExecuteReader();
+
+            return Ok("Executed vulnerable SQL query");
+        }
+
     }
 }
