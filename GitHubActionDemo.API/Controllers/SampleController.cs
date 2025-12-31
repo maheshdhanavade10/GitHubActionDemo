6d35feea7848cdc09e1a07521989a91671b4b4e9
maheshdhanavade10 <mahesh.dhanavade@gmail.com>
2025-12-31 14:24:15 +0530

    adding controller

---
+++ GitHubActionDemo.API/Controllers/SampleController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitHubActionDemo.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        //generate sample get request with ok response
+        [HttpGet("auth")]
+        public IActionResult GetAuthTest()
+        {
+            return Ok(new { Message = "Auth test successful" });
+        }
+    }
+}
